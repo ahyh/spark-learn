5de7ef711b51370505a90146e510d4f8c71adee4
ahyanhuan <ahyanhuan@sina.com>
2019-01-20 23:01:36 +0800

    spark实现wordcount

---
+++ src/main/scala/com/yh/spark/learn/WordCountTest.scala
@@ -0,0 +1,36 @@
+package com.yh.spark.learn
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * spark实现word-count
+  */
+object WordCountTest {
+
+  def main(args: Array[String]): Unit = {
+    //创建spark执行入口
+    val conf = new SparkConf()
+    val sc = new SparkContext(conf)
+    //指定以后从哪里读取数据集，创建RDD
+    //    sc.textFile(args(0)).flatMap(_.split(" "))
+    //      .map((_, 1)).reduceByKey(_ + _)
+    //      .sortBy(_._2, false).saveAsTextFile(args(1))
+
+    //加上类型
+    val lines: RDD[String] = sc.textFile(args(0))
+    //切分压平
+    val words: RDD[String] = lines.flatMap(_.split(" "))
+    //将单词合一组合
+    val wordAndOne: RDD[(String, Int)] = words.map((_, 1))
+    //按key进行聚合
+    val reduced: RDD[(String, Int)] = wordAndOne.reduceByKey(_ + _)
+    //排序
+    val sorted: RDD[(String, Int)] = reduced.sortBy(_._2, false)
+    //将结果保存在HDFS中
+    sorted.saveAsTextFile(args(1))
+    //释放资源
+    sc.stop()
+  }
+
+}
